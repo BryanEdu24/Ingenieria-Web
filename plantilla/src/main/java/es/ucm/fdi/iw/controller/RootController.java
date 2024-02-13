cf87d41be8223e4d7ce7a4c345ed63c46de2ffab
clarasot22 <clarasot@ucm.es>
2024-02-13 13:52:52 +0100

    Plantilla añadida con las vistas
    Co-Authored-By: David <100686200+DavidChGA@users.noreply.github.com>
    Co-Authored-By: Edu <47254221+BryanEdu24@users.noreply.github.com>
    Co-Authored-By: LucasBravo01 <144778411+LucasBravo01@users.noreply.github.com>

--- plantilla/src/main/java/es/ucm/fdi/iw/controller/RootController.java
+++ plantilla/src/main/java/es/ucm/fdi/iw/controller/RootController.java
@@ -7,20 +7,50 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
- *  Non-authenticated requests only.
+ * Non-authenticated requests only.
  */
 @Controller
 public class RootController {
 
-	private static final Logger log = LogManager.getLogger(RootController.class);
+    private static final Logger log = LogManager.getLogger(RootController.class);
 
-	@GetMapping("/login")
+    @GetMapping("/login")
     public String login(Model model) {
         return "login";
     }
 
-	@GetMapping("/")
+    @GetMapping("/")
     public String index(Model model) {
         return "index";
+    }
+
+    @GetMapping("/home1")
+    public String TM_home1(Model model) {
+        return "TM_home1";
+    }
+
+    @GetMapping("/home2")
+    public String TM_home2(Model model) {
+        return "TM_home2";
+    }
+
+    @GetMapping("/tareas")
+    public String TM_tareas(Model model) {
+        return "TM_task";
+    }
+
+    @GetMapping("/jefe")
+    public String TM_jefe(Model model) {
+        return "TM_jefe";
+    }
+
+    @GetMapping("/gastos")
+    public String TM_gastos(Model model) {
+        return "TM_expenses";
+    }
+
+    @GetMapping("/admin")
+    public String TM_admin(Model model) {
+        return "TM_admin";
     }
 }
