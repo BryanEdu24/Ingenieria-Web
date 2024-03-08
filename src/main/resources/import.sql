c002b54ccb4c3fa60ef18860f08931a734093dc8
DavidChGA <100686200+DavidChGA@users.noreply.github.com>
2024-03-08 16:13:01 +0100

    Cambios Insert

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,9 +2,6 @@
 INSERT INTO IWUser (id, enabled, roles, username, password)
 VALUES (1, TRUE, 'ADMIN,USER', 'a',
     '{bcrypt}$2a$10$2BpNTbrsarbHjNsUWgzfNubJqBRf.0Vz9924nRSHBqlbPKerkgX.W');
-INSERT INTO IWUser (id, enabled, roles, username, password)
-VALUES (2, TRUE, 'USER', 'b',
-    '{bcrypt}$2a$10$2BpNTbrsarbHjNsUWgzfNubJqBRf.0Vz9924nRSHBqlbPKerkgX.W');
 
 INSERT INTO House (id, enabled, name, pass)
 VALUES (1, TRUE, 'HOUSE_1', '{bcrypt}$2a$10$2BpNTbrsarbHjNsUWgzfNubJqBRf.0Vz9924nRSHBqlbPKerkgX.W');
@@ -17,6 +14,11 @@ VALUES (2, TRUE, 'Baño', 1);
 
 INSERT INTO Room (id, enabled, name, house_id)
 VALUES (3, TRUE, 'Habitación', 1);
+
+INSERT INTO IWUser (id, enabled, roles, username, password, house_id)
+VALUES (2, TRUE, 'USER', 'b',
+    '{bcrypt}$2a$10$2BpNTbrsarbHjNsUWgzfNubJqBRf.0Vz9924nRSHBqlbPKerkgX.W', 1);
+
 
 -- start id numbering from a value that is larger than any assigned above
 ALTER SEQUENCE "PUBLIC"."GEN" RESTART WITH 1024;
